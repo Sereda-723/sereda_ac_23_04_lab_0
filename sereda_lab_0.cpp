ad993ac7eb09b9caf3dcce212a7e10ec73a6e7f4
Vladimir_Sereda_laptop <vovasereda541@gmail.com>
2024-09-05 13:16:47 +0300

    Enter x

--- sereda_lab_0.cpp
+++ sereda_lab_0.cpp
@@ -9,5 +9,18 @@ int main() {
 
     cout << "Hello world!!!" << endl;
         
+    // vector <string> sp = {"1", "2", "3", "4", "5", "6", "7"};
+
+    // for (string element : sp) {
+    //     cout << element << endl;
+    // }
+
+    int x;
+    cin >> x;
+    if (x == 0) {
+        cout << "error"s << endl;
+    } else {
+        cout << 10 / x;
+    }
     return 0;
 }
